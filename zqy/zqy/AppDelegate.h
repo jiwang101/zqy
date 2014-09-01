30c0f1c12f08367d1792d52673740570e3ec69fc
xiongjianming <daoyi@daoyiccf.local>
2014-09-01 14:59:14 +0800

    new hello

---
+++ zqy/zqy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zqy
+//
+//  Created by daoyi on 14-9-1.
+//  Copyright (c) 2014年 daoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ zqy/zqy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  zqy
+//
+//  Created by daoyi on 14-9-1.
+//  Copyright (c) 2014年 daoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
